7dd12f66db2ef38f39829f1dc0f7e6b8caaf66e1
unknown <lofman.andreas@gmail.com>
2012-10-05 15:10:45 +0200

    Merge

---
+++ src/main/java/edu/chl/tbook/TBook.java
@@ -0,0 +1,37 @@
+package edu.chl.tbook;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * The complete shop as a singleton
+ * 
+ * NOTE: Uses Java 1.7
+ *
+ * @author hajo
+ */
+public enum TBook {
+
+    INSTANCE;
+    /*private final IProductCatalogue productCatalogue = ProductCatalogue.newInstance("shop_pu");
+    private final ICustomerRegistry customerRegistry = CustomerRegistry.newInstance("shop_pu");
+    private final IOrderBook orderBook = OrderBook.newInstance("shop_pu");*/
+
+    private TBook() {
+        Logger.getAnonymousLogger().log(Level.INFO, "TBook alive {0}", this.hashCode());
+
+    }
+    /*
+    public ICustomerRegistry getCustomerRegistry() {
+        return customerRegistry;
+    }
+
+    public IOrderBook getOrderBook() {
+        return orderBook;
+    }
+
+    public IProductCatalogue getProductCatalogue() {
+        return productCatalogue;
+    }
+    * */
+}
