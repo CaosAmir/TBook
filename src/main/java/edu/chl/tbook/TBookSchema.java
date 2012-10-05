33e9e4f4239534801a1a15eb040ca4a9e06eadaf
Kristofer Tapper <kristofer90@hotmail.com>
2012-10-05 15:13:46 +0200

    'update1'

---
+++ src/main/java/edu/chl/tbook/TBookSchema.java
@@ -0,0 +1,76 @@
+package edu.chl.tbook;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.*;
+
+/**
+ *
+ * @author Kristofer
+ */
+
+@Entity
+public class TBookSchema implements Serializable{
+
+    
+    @Id
+    @OneToOne
+    private TBookUser owner;
+    @ManyToMany
+    private Exercise ex;
+    private Long datum;
+    private String comment;
+
+    public TBookSchema() {
+    }
+
+    public TBookSchema(String comment, TBookUser owner, Exercise ex) {
+        this.datum = Calendar.getInstance().getTimeInMillis();
+        this.comment = comment;
+        this.owner = owner;
+        this.ex = ex;
+    }
+
+    public TBookSchema(String comment, TBookUser owner) {
+        this.datum = Calendar.getInstance().getTimeInMillis();
+        this.comment = comment;
+        this.owner = owner;
+    }
+    
+    
+    
+    
+    public Long getDate() {
+        return datum;
+    }
+
+    public void setDate(Long datum) {
+        this.datum = datum;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public TBookUser getUser() {
+        return owner;
+    }
+
+    public void setUser(TBookUser owner) {
+        this.owner = owner;
+    }
+
+    public Exercise getEx() {
+        return ex;
+    }
+
+    public void setEx(Exercise ex) {
+        this.ex = ex;
+    }
+    
+    
+}
