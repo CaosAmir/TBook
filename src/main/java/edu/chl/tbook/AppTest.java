da3c2ee74de8a76eb152557b08a2e789e3c31808
unknown <lofman.andreas@gmail.com>
2012-10-05 14:23:19 +0200

    Fixed

--- src/main/java/edu/chl/tbook/AppTest.java
+++ src/main/java/edu/chl/tbook/AppTest.java
@@ -4,10 +4,14 @@ package edu.chl.tbook;
  * Hello world!
  *
  */
-public class App 
+public class AppTest 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello!" );
+    }
+    
+    public void test(){
+        System.out.println("Test!");
     }
 }
